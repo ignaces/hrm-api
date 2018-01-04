3767dd99c901390200091e1fed3b52186dc365a9
Andrés Santa María <aletheialego@gmail.com>
2018-01-03 23:58:30 -0300

    personas y facsimiles persona

--- app/Controllers/Http/Evaluacion/Instrumento.js
+++ app/Controllers/Http/Evaluacion/Instrumento.js
@@ -10,7 +10,7 @@ class Instrumento {
       var id = request.input("idInstrumento");
       
       const query = `call creaFacsimil('${id}')`;
-      const facsimil   = await Database.connection('local').schema.raw(query);
+      const facsimil   = await Database.connection('dev').schema.raw(query);
       /**
        * 
        * Crear facsimil
@@ -19,7 +19,19 @@ class Instrumento {
        */
       response.json(facsimil[0][0]);
     }
+    
+    async getFacsimilesPersona({request,response}){
+        var id = request.input("idCliente");
+        var idProcesoPersona = request.input("idProcesoPersona");
+        var codigoInstrumento = request.input("codigoInstrumento");
+        
+        const facsimiles = [
+            {id:'866d2b50-f0f0-11e7-bf12-bc764e100f2b'},
+            {id:'595299cc-ef15-11e7-80ab-a7d2274a0124'}
+        ];
 
+        response.json(facsimiles);
+    }
     async getFacsimil({request,response}){
     
         var id = request.input("idFacsimil");
