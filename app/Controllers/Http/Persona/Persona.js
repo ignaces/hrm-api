bce7a3e8426cf4a5624334bc0e36cfb7b7563e69
jcoruiz <jcoruiz@gmail.com>
2018-01-10 15:22:39 -0300

    weas

--- app/Controllers/Http/Persona/Persona.js
+++ app/Controllers/Http/Persona/Persona.js
@@ -15,6 +15,34 @@ class Persona {
       
       response.json(usp[0][0]);
     }
+
+    async getPersona({request,response}){
+      
+        const idCliente = request.input('idCliente');
+        const idPersona = request.input('idPersona');
+        
+        const query = `call pers_getPersona('${idPersona}')`;
+        
+        const result = await Database.connection('dev').schema.raw(query);
+
+        const body = 
+        {
+          estado: {
+            codigo: "",
+            mensaje: ""
+          },
+          paginacion: {
+            totalRegistros: "",
+            totalPaginas: "",
+            pagina: "",
+            registros: ""
+          },
+          data: {persona: result[0][0]}
+          
+        }
+        
+        response.json(body);
+      }
     
 }
 
