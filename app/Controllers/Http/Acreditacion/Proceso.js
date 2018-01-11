357aaf91669b9c57f18d64fe042f503e7da26166
Andrés Santa María <aletheialego@gmail.com>
2018-01-11 16:41:25 -0300

    TCO

--- app/Controllers/Http/Acreditacion/Proceso.js
+++ app/Controllers/Http/Acreditacion/Proceso.js
@@ -45,9 +45,10 @@ class Proceso {
                 apellidoMaterno:persona.apellidoMaterno,
                 identificador:persona.identificador,
                 tiposInstrumentos:[
-                    {id:"340f82f6-f0fa-11e7-bf12-bc764e100f2b",codigoInterno:"EIC",nombre:"Entrevista Insidentes Críticos",idCompetenciaTipo:"87ed8c12-f0e7-11e7-bf12-bc764e100f2b"},
-                    {id:"342859c7-f0fa-11e7-bf12-bc764e100f2b",codigoInterno:"SOT",nombre:"Set de Observación en Terreno",idCompetenciaTipo:"87ed8c12-f0e7-11e7-bf12-bc764e100f2b"},
-                    {id:"9ff833c8-f0e7-11e7-bf12-bc764e100f2b",codigoInterno:"TCO",nombre:"Instrumento TCO 1",idCompetenciaTipo:"87d4b07c-f0e7-11e7-bf12-bc764e100f2b"}]
+                    {id:"340f82f6-f0fa-11e7-bf12-bc764e100f2b",codigoInterno:"EIC",nombre:"Entrevista Insidentes Críticos",idCompetenciaTipo:"87ed8c12-f0e7-11e7-bf12-bc764e100f2b",idPersonaProceso:persona.idPersonaProceso},
+                    {id:"342859c7-f0fa-11e7-bf12-bc764e100f2b",codigoInterno:"SOT",nombre:"Set de Observación en Terreno",idCompetenciaTipo:"87ed8c12-f0e7-11e7-bf12-bc764e100f2b",idPersonaProceso:persona.idPersonaProceso},
+                    //{id:"9ff833c8-f0e7-11e7-bf12-bc764e100f2b",codigoInterno:"TCO",nombre:"Instrumento TCO 1",idCompetenciaTipo:"87d4b07c-f0e7-11e7-bf12-bc764e100f2b",idPersonaProceso:persona.idPersonaProceso}
+                ]
                 
             }
         }).toArray()
