0096ca080d194483154a7e2762f64d6a64ef37d9
Andrés Santa María <aletheialego@gmail.com>
2018-01-18 10:51:44 -0300

    commentario

--- app/Controllers/Http/Persona/Persona.js
+++ app/Controllers/Http/Persona/Persona.js
@@ -11,7 +11,7 @@ class Persona {
       const usp   = await Database.connection('dev').schema.raw(query);
       
       //const usp   = yield Database.schema.raw("SELECT * from users;");
-      //response.json(usp[0]);
+      //response.json(usp[0]);--
       
       response.json(usp[0][0]);
     }
