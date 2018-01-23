3f7b2c0674110c01b56caf6797e9725508921008
= <=>
2018-01-23 00:10:03 -0300

    log en base de datos

--- app/Middleware/Logging.js
+++ app/Middleware/Logging.js
@@ -1,17 +1,31 @@
 'use strict'
-
+const data = use('App/Utils/Data')
+const uuidv1 = require('uuid/v1');
 class Logging {
   async handle ({request,response}, next) {
     
     try {
-      console.log("module=>",request.params.module);
-      console.log("controller=>",request.params.controller);
-      console.log("action=>",request.params.action);
-      console.log("cliente=>",request.input("cliente"));
-      console.log("params=>",request.all()); 
+      var codigo = uuidv1();
+
+      var cliente = request.input('cliente');
+      var idusers = '';
+      var module = request.params.module;
+      var controller = request.params.controller;
+      var action = request.params.action;
+      var params = JSON.stringify(request.all());
+      var type = 'CALL';
+
+      const query =`call core_addLogApi('${codigo}', '${cliente}', '${idusers}',  '${type}', '${module}', '${controller}', '${action}', '${params}')`;
+      const respuesta   = await data.execQuery(cliente,query);
+
+      // console.log("module=>",request.params.module);
+      // console.log("controller=>",request.params.controller);
+      // console.log("action=>",request.params.action);
+      // console.log("cliente=>",request.input("cliente"));
+      // console.log("params=>",request.all()); 
       
     } catch (error) {
-      response.redirect('/login');
+      console.log(error);
     }
    
 
