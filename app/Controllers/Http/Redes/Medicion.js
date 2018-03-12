52fe53cf3cde77d8db3a5ec63f73afe0e7c88930
Andres Santa Maria Norero <andres.santamaria@asantamaria-mac.local>
2018-03-12 10:47:59 -0300

    des

--- app/Controllers/Http/Redes/Medicion.js
+++ app/Controllers/Http/Redes/Medicion.js
@@ -2,6 +2,7 @@
 const Database = use('Database')
 const got = use('got')
 const data = use('App/Utils/Data')
+const Env = use('Env')
 /**
  * asdasdadasdadasda
  * @class
@@ -19,7 +20,47 @@ class Medicion {
         
         response.json(usp[0][0]);
     }
-
+    async getGraph ({request,response}){
+        const cliente =request.input('cliente');
+        var neo4j = Env.get('NEO4J', '192.168.3.18:7474');
+        var neo4jUser = Env.get('NEO4J_USER', 'neo4j');
+        var neo4jPassword = Env.get('NEO4J_PASSWORD', 'Qwerty123');
+        var options_auth = new Buffer(`${neo4jUser}:${neo4jPassword}`).toString("base64")
+        
+  
+        
+        /**
+         * Get Preguntas
+         */
+        var idPersonas=""
+        var instrucciones ={statements:[]}
+        
+        /**
+         * Se crea persona que está contestando
+         */
+        instrucciones.statements.push({
+          statement : `MERGE (p:Persona { codigo:'${code}' }) \
+                      ON CREATE SET p.nombre = '${persona.nombres}', \
+                      p.apellidoPaterno = '${persona.apellidoPaterno}' , \
+                      p.apellidoMaterno = '${persona.apellidoMaterno}' \
+                      ON MATCH SET p.nombre = '${persona.nombres}', \
+                      p.apellidoPaterno = '${persona.apellidoPaterno}' , \
+                      p.apellidoMaterno = '${persona.apellidoMaterno}'`                      
+        })
+     
+        const rPersonas = await got.post(`http://${neo4j}/db/data/transaction/commit`,
+          {
+            
+            json:true,
+            body: instrucciones,
+            headers:{
+              'Authorization': "Basic "+options_auth
+            }      
+          })
+          //console.log(rPersonas.body.results)
+          return rPersonas.body
+  
+      }
 }
 module.exports = Medicion
 
