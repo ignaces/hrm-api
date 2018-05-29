f9b8d28e125dddbbb3e4cb01506808b8542057a9
Andres Santa Maria Norero <andres.santamaria@asantamaria-mac.local>
2018-05-29 18:35:17 -0400

    acciones

--- app/Controllers/Http/Talento/Persona.js
+++ app/Controllers/Http/Talento/Persona.js
@@ -4,7 +4,7 @@ const got = use('got')
 var Enumerable = require('linq')
 const data = use('App/Utils/Data')
 const permisos = use('App/Controllers/Http/Core/Permisos')
-
+const dateformat = require('dateformat');
 class Persona {
     async getPersona({request,response}){
     
@@ -56,6 +56,7 @@ class Persona {
             pais:posicion.pais,
             nombreNacionalidad:posicion.nombreNacionalidad,
             iconoPais:posicion.iconoPais,
+            fechaNacimiento: dateformat(posicion.fechaNacimiento,'dd-mm-yyyy'),
             atributos:Enumerable.from(result[0][0]).where(`$.idPosicion == "${posicion.idPosicion}"`).select(function(atributo){
                 return {
                     atributo:atributo.atributo,
@@ -67,16 +68,56 @@ class Persona {
 
         }
     }).toArray()
-    /*
-codigoIdioma
-idioma
-banderaIdioma
-nivelIdioma
-pais
-nombreNacionalidad 
-iconoPais
-     */
+  
     response.json(persona);
+    }
+
+    async getPosiblesSucesores({request,response}){
+    
+        var idPosicion = request.input('idPosicion');
+        var idProceso = request.input('idProceso');
+        var identificador = "";
+        
+        const cliente = request.input('cliente');
+        
+        const query =  `call tale_getPosiblesSucesores('${idProceso}','${idPosicion}')`;
+        
+        
+        const result   = await data.execQuery(cliente,query);
+
+        const posiciones = Enumerable.from(result[0][0]).distinct("$.idPosicion").select(function(posicion){
+            return{
+                idPosicion:posicion.idPosicion,
+                nombre:posicion.nombre,
+                codigo:posicion.codigo,
+                critico:posicion.critico,
+                nivel:posicion.nivel,
+                idPadre:posicion.idPadre,
+                idPersona:posicion.idPersona,
+                nombresPersona:posicion.nombresPersona,
+                apellidoPaterno:posicion.apellidoPaterno,
+                apellidoMaterno:posicion.apellidoMaterno,
+                fotoPersona:posicion.fotoPersona,
+                colorPosicion:posicion.colorPosicion,
+                nombreCuadrante:posicion.nombreCuadrante,
+                valor:posicion.valor,
+                edd:posicion.edd,
+                idCuadranteEq:posicion.idCuadranteEq,
+                valorEdeEq:posicion.valorEdeEq,
+                idCuadrante:posicion.idCuadrante,
+                atributos:Enumerable.from(result[0][0]).where(`$.idPosicion == "${posicion.idPosicion}"`).select(function(atributo){
+                    return {
+                        atributo:atributo.atributo,
+                        colorAtributo:atributo.colorAtributo,
+                        iconoAtributo:atributo.iconoAtributo,
+                        tooltipAtributo:atributo.tooltipAtributo,
+                    }
+                }).toArray()
+
+            }
+        }).toArray()
+        
+        response.json(posiciones);
     }
 }
 
