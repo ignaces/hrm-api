9d01bdc4eff683a6935bb38d78638fe46267239a
Andres Santa Maria Norero <andres.santamaria@asantamaria-mac.local>
2018-05-31 17:46:08 -0400

    acciones

--- app/Controllers/Http/Talento/Accion.js
+++ app/Controllers/Http/Talento/Accion.js
@@ -14,7 +14,6 @@ class Accion {
         
         const query =  `call tale_getAccionesPredefinidas()`;
         
-        
         const result   = await data.execQuery(cliente,query);
 
         const acciones = result[0][0];
@@ -34,6 +33,102 @@ class Accion {
         const competencias = result[0][0];
         
         response.json(competencias);
+    }
+
+    async getPlanDesarrollo({request,response}){
+        const idProceso = request.input('idProceso');
+        const idPersona = request.input('idPersona');
+        const cliente = request.input('cliente');
+        
+        var query =  `call tale_getPlanDesarrolloPersona('${idProceso}','${idPersona}')`;
+        
+        var resultPlan   = await data.execQuery(cliente,query);
+        
+        if(resultPlan[0][0].length==0){
+            query =  `call tale_addPlanDesarrolloPersona('${idProceso}','${idPersona}')`;
+        
+            resultPlan   = await data.execQuery(cliente,query);
+        }
+        
+
+
+        const plan = {
+            idPlan:resultPlan[0][0][0].idPlan,
+            acciones:Enumerable.from(resultPlan[0][0]).distinct("$.idAccion").select(function(accion){
+                return{
+                        id:accion.idAccion,
+                        nombre:accion.nombre,
+                        objetivo:accion.objetivo,
+                        idCompetencia:accion.idCompetencia,
+                        competencia:accion.competencia,
+                        apoyoGerente:accion.apoyoGerente,
+                        fechaInicio:accion.fechaInicio,
+                        fechaTermino:accion.fechaTermino,
+                        indicadores:Enumerable.from(resultPlan[0][0]).where(`$.idAccion=="${accion.idAccion}"`).select(function(indicador){
+                            return{
+                                id:indicador.idIndicador,
+                                texto:indicador.indicador
+                            }}).toArray()
+                }}).toArray()
+        };
+
+        response.json(plan);
+    }
+    async getPlanDesarrolloById({request,response}){
+        const idPlan = request.input('idPlan');
+        
+        const cliente = request.input('cliente');
+        
+        var query =  `call tale_getPlanDesarrolloPersonaById('${idPlan}')`;
+        
+        var resultPlan   = await data.execQuery(cliente,query);
+        
+       
+        const plan = {
+            idPlan:resultPlan[0][0][0].idPlan,
+            acciones:Enumerable.from(resultPlan[0][0]).distinct("$.idAccion").select(function(accion){
+                return{
+                        id:accion.idAccion,
+                        nombre:accion.nombre,
+                        objetivo:accion.objetivo,
+                        idCompetencia:accion.idCompetencia,
+                        competencia:accion.competencia,
+                        apoyoGerente:accion.apoyoGerente,
+                        fechaInicio:accion.fechaInicio,
+                        fechaTermino:accion.fechaTermino,
+                        indicadores:Enumerable.from(resultPlan[0][0]).where(`$.idAccion=="${accion.idAccion}"`).select(function(indicador){
+                            return{
+                                id:indicador.idIndicador,
+                                texto:indicador.indicador
+                            }}).toArray()
+                }}).toArray()
+        };
+
+        response.json(plan);
+    }
+
+    async addAccion({request,response}){
+        const idProceso = request.input('idProceso');
+        const idPlan = request.input('idPlan');
+        const accion = request.input('accion').accion;
+
+        const cliente = request.input('cliente');
+        
+        const query =  `call tale_addAccion('${idPlan}',0,'${accion.objetivo}','${accion.apoyoGerente}','${accion.idCompetencia}','${accion.idAccionPredeterminada}','${accion.fechaInicio}','${accion.fechaTermino}')`;
+        
+        
+        const result   = await data.execQuery(cliente,query);
+
+        const rAccion = result[0][0][0];
+
+        var idAccion = rAccion.id;
+        for(var i in accion.indicadores){
+            const queryIndicador =  `call tale_addIndicador('${idAccion}','${accion.indicadores[i].texto}')`;
+        
+            const resultIndicador   = await data.execQuery(cliente,queryIndicador);
+        }
+        
+        response.json(rAccion);
     }
 }
 module.exports =Accion;
