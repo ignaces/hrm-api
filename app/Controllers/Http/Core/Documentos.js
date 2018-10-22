d8c4476adff05e0c6631133d2f2c83a43bc4e204
enovum <mauricio.rodriguez@fch.cl>
2018-10-22 17:01:05 -0300

    docs

--- app/Controllers/Http/Core/Documentos.js
+++ app/Controllers/Http/Core/Documentos.js
@@ -13,14 +13,32 @@ class Documentos {
         const cliente =request.input('cliente') ;
         const query =  `call getDocumentos('${activo}')`;
         const respuesta   = await data.execQuery(cliente,query);
-        
+        const docs = Enumerable.from(respuesta[0][0]).distinct("$.idCategoria").select(function(categoria){
+       
+            return{
+                    idCategoria:categoria.id,
+                    categoria:categoria.categoria,
+                    documentos:Enumerable.from(respuesta[0][0]).where(`$.idCategoria == "${categoria.idCategoria}"`).select(function(doc){
+                        return{
+                            id:doc.id,
+                            nombre:doc.nombre,
+                            tipo:doc.tipo,
+                            url:doc.url,
+                            activo:doc.activo,
+                            dt_cre:doc.dt_cre,
+                            dt_mod:doc.dt_mod
+                        }
+                    }).toArray()
+
+                }
+            }).toArray();
         response.json({
             "estado": {
                 "codigo": "OK",
                 "mensaje": ""
             },
             "paginacion": "",
-            "data": respuesta[0][0]
+            "data": docs
         });
     }
 
