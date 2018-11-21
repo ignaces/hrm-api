a6c25a7f8a03cf60500d8d794f259129b921297f
Jorge Luis Leiva Silva <jlleivas@vectoritcgroup.com>
2018-11-21 15:37:11 -0300

    refactoring calibracion

--- app/Controllers/Http/Desempeno/Proceso.js
+++ app/Controllers/Http/Desempeno/Proceso.js
@@ -503,6 +503,15 @@ class Proceso {
         });
     }
 
+    async updateCalibracion({request,response}){
+        var idEdeEtapaTareaActor=request.input('idEdeEtapaTareaActor');
+        var calibracion=request.input('calibracion');
+        const cliente =request.input('cliente') ;
+
+        const query =  `call ede_updateCalibracion('${idEdeEtapaTareaActor}','${calibracion}')`;
+        const respuesta   = await data.execQuery(cliente,query);
+    }
+
     async getListaCalibracion({request,response}){
         var idEtapa=request.input('idEtapa')
         var idPersonaActor=request.input('idPersonaActor')
@@ -583,56 +592,43 @@ class Proceso {
                 const queryResultado = `call ede_calculaEvaluacion('${idOpinante}')`;
                 console.log('idOpinante:'+idOpinante);
             
-                const resultado2  = await data.execQuery(cliente,queryResultado);
-                
-                var resultadoCompetencias={nivel:"No Disponible"};
-                var resultadoMetas = {nivel:"No Disponible"};
-                var resultadoGlobal = {nivel:"No Disponible"};
-                var resultadoCalibracion = evaluados[evaluado].resultadoCalibracion;
+                var resultadoGlobal = evaluados[evaluado].tareas[0].tarea.resultadoEvaluacion;
+                var resultadoCalibracion = evaluados[evaluado].tareas[0].tarea.resultadoCalibracion;
                 console.log(JSON.stringify(evaluados[evaluado].resultadoCalibracion));
-                if(resultado2[0][0][0]!=undefined){
-                    resultadoCompetencias = resultado2[0][0][0];
-                }
-                if(resultado2[0][1][0]!=undefined){
-                    resultadoMetas = resultado2[0][1][0];
-                }
-                if(resultadoMetas.nivel!="No Disponible" && resultadoCompetencias.nivel!="No Disponible"){
-                    resultadoGlobal = {nivel:`${resultadoCompetencias.nivel}${resultadoMetas.nivel}`};
-                }
-                evaluados[evaluado].tareas[0].resultadoGlobal = resultadoGlobal;
-                if(resultadoGlobal.nivel == "AA"){
+                
+                if(resultadoGlobal == "AA"){
                     matrizEval.AA++;
                     matrizEval.sum++;
                     matrizEval.Sesp++;
-                }else if(resultadoGlobal.nivel == "AB"){
+                }else if(resultadoGlobal == "AB"){
                     matrizEval.AB++;
                     matrizEval.sum++;
                     matrizEval.Sesp++;
-                }else if(resultadoGlobal.nivel == "AC"){
+                }else if(resultadoGlobal == "AC"){
                     matrizEval.AC++;
                     matrizEval.sum++;
                     matrizEval.Besp++;
-                }else if(resultadoGlobal.nivel == "BA"){
+                }else if(resultadoGlobal == "BA"){
                     matrizEval.BA++;
                     matrizEval.sum++;
                     matrizEval.Sesp++;
-                }else if(resultadoGlobal.nivel == "BB"){
+                }else if(resultadoGlobal == "BB"){
                     matrizEval.BB++;
                     matrizEval.sum++;
                     matrizEval.esp++;
-                }else if(resultadoGlobal.nivel == "BC"){
+                }else if(resultadoGlobal == "BC"){
                     matrizEval.BC++;
                     matrizEval.sum++;
                     matrizEval.Besp++;
-                }else if(resultadoGlobal.nivel == "CA"){
+                }else if(resultadoGlobal == "CA"){
                     matrizEval.CA++;
                     matrizEval.sum++;
                     matrizEval.Besp++;
-                }else if(resultadoGlobal.nivel == "CB"){
+                }else if(resultadoGlobal == "CB"){
                     matrizEval.CB++;
                     matrizEval.sum++;
                     matrizEval.Besp++;
-                }else if(resultadoGlobal.nivel == "CC"){
+                }else if(resultadoGlobal == "CC"){
                     matrizEval.CC++;
                     matrizEval.sum++;
                     matrizEval.Besp++;
