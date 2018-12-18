6256dc61c401c182dd0106b57149f2dfce08a763
juandpm <juand.pm1992@gmail.com>
2018-12-17 22:53:33 -0300

    Feedback Accion 1

--- app/Controllers/Http/Feedback/Persona.js
+++ app/Controllers/Http/Feedback/Persona.js
@@ -66,6 +66,91 @@ class Persona {
             "data": respuesta[0][0]
         });
     }
+    async saveFeedback({request,response}){
+       
+        var idFeedbackOpinante = request.input("idFeedbackOpinante");
+        var txtNomPlan = request.input("txtNomPlan");
+        var txtAccion = request.input("txtAccion");
+        var txtObjetivo = request.input("txtObjetivo");
+        var fechaInicio = request.input("fechaInicio"); 
+        
+        const cliente =request.input('cliente') ;
+        const query =  `call feedback_savePlanFeedback('${idFeedbackOpinante}','${txtNomPlan}','${txtAccion}','${txtObjetivo}','${fechaInicio}')`;
+        
+        const respuesta   = await data.execQuery(cliente,query);
+        
+        response.json({
+            "estado": {
+                "codigo": "OK",
+                "mensaje": ""
+            },
+            "paginacion": "",
+            "data": respuesta[0][0]
+        });
+    }
+    async getAcciones({request,response}){
+       
+        var idFeedbackOpinante = request.input("idFeedbackOpinante");
+        
+        const cliente =request.input('cliente') ;
+        const query =  `call feedback_getAccionFeedback('${idFeedbackOpinante}')`;
+        
+        const respuesta   = await data.execQuery(cliente,query);
+        
+        response.json({
+            "estado": {
+                "codigo": "OK",
+                "mensaje": ""
+            },
+            "paginacion": "",
+            "data": respuesta[0][0]
+        });
+    }
+    async addAccion({request,response}){
+       
+        var idFeedbackOpinante = request.input("idFeedbackOpinante");
+        var objAccion = request.input("objAccion");
+
+        var txtAccion = objAccion.accion;
+        var txtObjetivo = objAccion.objetivo;
+        var fechaTermino = objAccion.fechaTermino;
+        
+        const cliente =request.input('cliente') ;
+        const query =  `call feedback_saveAccionFeedback('${idFeedbackOpinante}','${txtAccion}','${txtObjetivo}','${fechaTermino}')`;
+        
+        const respuesta   = await data.execQuery(cliente,query);
+        
+        response.json({
+            "estado": {
+                "codigo": "OK",
+                "mensaje": ""
+            },
+            "paginacion": "",
+            "data": respuesta[0][0]
+        });
+    }
+    async deleteAccion ({request,response}){
+       
+        var idFeedbackOpinante = request.input("idFeedbackOpinante");
+        var objAccion = request.input("objAccion");
+
+        var idAccion = objAccion.id;
+
+        const cliente =request.input('cliente') ;
+        const query =  `call feedback_deleteAccionFeedback('${idFeedbackOpinante}','${idAccion}')`;
+        
+        const respuesta = await data.execQuery(cliente,query);
+
+        response.json({
+            "estado": {
+                "codigo": "OK",
+                "mensaje": ""
+            },
+            "paginacion": "",
+            "data": respuesta[0][0]
+        });
+    }
+
 
 
 }
